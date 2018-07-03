3c9c16cc42da9389aae16b21c347db5b160b863c
jelleas <jelle.van.assema@gmail.com>
2018-07-03 12:58:35 -0400

    summarize submit50

--- push50.py
+++ push50.py
@@ -1,7 +1,11 @@
 import sys
 import time
 import contextlib
+import requests
 from threading import Thread
+
+class Error(Exception):
+    pass
 
 @contextlib.contextmanager
 def progress(message):
@@ -13,6 +17,7 @@ def progress(message):
             sys.stdout.write(".")
             sys.stdout.flush()
             time.sleep(0.5)
+        print()
 
     try:
         progressing = True
@@ -23,5 +28,46 @@ def progress(message):
         progressing = False
         thread.join()
 
-with progress("connecting"):
-    time.sleep(5)
+def check_announcements():
+    """Check for any announcements from cs50.me, raise Error if so"""
+    res = requests.get("https://cs50.me/status/submit50") # TODO change this to submit50.io!
+    if res.status_code == 200 and res.text.strip():
+        raise Error(res.text.strip())
+
+def push(org, branch, sentinel = None):
+    # TODO check announcements
+    # TODO check for git 2.7
+
+    with progress("Connecting"):
+        # TODO TODO move, decide on commit name
+        # TODO check version vs submit50.io (or cs50.me)
+        if sentinel:
+            # TODO ensure sentinel exists at org/repo/branch
+            pass
+        # TODO ensure .push50.yaml exists at org/repo/branch
+        # TODO parse .push50.yaml
+        # TODO check for missing files
+        pass
+
+    with progress("Authenticating"):
+        # TODO authenticate
+        pass
+
+    with progress("Preparing"):
+        # TODO clone bare
+            # TODO check for any permission errors: CS50.me / wrong username
+        # TODO .gitattribute stuff
+        # TODO git config
+        # TODO add files to staging area
+        # TODO git lfs
+        # TODO check that at least 1 file is staged
+        pass
+
+    # TODO Submit50 special casing was here (academic honesty)
+
+    with progress("Uploading"):
+        # TODO commit + push
+        pass
+
+# example check50 call
+push("check50", "hello", sentinel = ".check50.yaml")
