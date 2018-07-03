331c296ad9199ad7066cba378ec29a8dd696d6ec
jelleas <jelle.van.assema@gmail.com>
2018-07-03 14:16:21 -0400

    added ProgressBar

--- push50.py
+++ push50.py
@@ -1,32 +1,45 @@
 import sys
 import time
-import contextlib
 import requests
 from threading import Thread
 
 class Error(Exception):
     pass
 
-@contextlib.contextmanager
-def progress(message):
+class ProgressBar:
     """Show a progress bar starting with message"""
-    def progress_runner():
-        sys.stdout.write(message + "...")
-        sys.stdout.flush()
-        while progressing:
-            sys.stdout.write(".")
-            sys.stdout.flush()
-            time.sleep(0.5)
-        print()
+    def __init__(self, message):
+        self._message = message
+        self._progressing = True
+        self._paused = False
+        self._thread = None
 
-    try:
-        progressing = True
-        thread = Thread(target=progress_runner)
-        thread.start()
-        yield
-    finally:
-        progressing = False
-        thread.join()
+    def pause(self):
+        """Pause the progress bar"""
+        self._paused = True
+
+    def unpause(self):
+        """Unpause the progress bar"""
+        self._paused = False
+
+    def __enter__(self):
+        def progress_runner():
+            sys.stdout.write(self._message + "...")
+            sys.stdout.flush()
+            while self._progressing:
+                if not self._paused:
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+                time.sleep(0.5)
+            print()
+
+        self._thread = Thread(target=progress_runner)
+        self._thread.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._progressing = False
+        self._thread.join()
 
 def check_announcements():
     """Check for any announcements from cs50.me, raise Error if so"""
@@ -38,7 +51,7 @@ def push(org, branch, sentinel = None):
     # TODO check announcements
     # TODO check for git 2.7
 
-    with progress("Connecting"):
+    with ProgressBar("Connecting"):
         # TODO TODO move, decide on commit name
         # TODO check version vs submit50.io (or cs50.me)
         if sentinel:
@@ -49,11 +62,11 @@ def push(org, branch, sentinel = None):
         # TODO check for missing files
         pass
 
-    with progress("Authenticating"):
+    with ProgressBar("Authenticating"):
         # TODO authenticate
         pass
 
-    with progress("Preparing"):
+    with ProgressBar("Preparing"):
         # TODO clone bare
             # TODO check for any permission errors: CS50.me / wrong username
         # TODO .gitattribute stuff
@@ -65,9 +78,18 @@ def push(org, branch, sentinel = None):
 
     # TODO Submit50 special casing was here (academic honesty)
 
-    with progress("Uploading"):
+    with ProgressBar("Uploading"):
         # TODO commit + push
         pass
 
 # example check50 call
 push("check50", "hello", sentinel = ".check50.yaml")
+
+"""
+with ProgressBar("Connecting") as progress_bar:
+    time.sleep(5)
+    progress_bar.pause()
+    time.sleep(5)
+    progress_bar.unpause()
+    time.sleep(5)
+"""
