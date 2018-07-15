8abf80e1f5d2a89f5f07852982ead38953137a7c
Chad Sharp <chad@cs50.harvard.edu>
2018-07-15 11:56:11 -0400

    removed keyring dependency

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
-from setuptools.command.install import install
+from setuptools.command.build_py import build_py
 
+import os
+import glob
 
 try:
     from babel.messages import frontend as babel
@@ -8,20 +10,19 @@ except ImportError:
     cmdclass = {}
 else:
     # https://stackoverflow.com/questions/40051076/babel-compile-translation-files-when-calling-setup-py-install
-    class InstallWithCompile(install):
+    class InstallWithCompile(build_py):
         def run(self):
             compiler = babel.compile_catalog(self.distribution)
             option_dict = self.distribution.get_option_dict("compile_catalog")
             compiler.domain = [option_dict["domain"][1]]
             compiler.directory = option_dict["directory"][1]
             compiler.run()
+            # os.system("tree")
+            # self.mo_files = glob.glob("**.mo", recursive=True)
+            # print(self.mo_files)
             super().run()
     cmdclass = {
-        "compile_catalog": babel.compile_catalog,
-        "extract_messages": babel.extract_messages,
-        "init_catalog": babel.init_catalog,
-        "update_catalog": babel.update_catalog,
-        "install": InstallWithCompile
+        "build_py": InstallWithCompile
     }
 
 
@@ -40,12 +41,13 @@ setup(
     },
     license="GPLv3",
     description="This is push50, CS50's internal library for using GitHub as data storage.",
-    install_requires=["attrs", "babel", "keyring", "pexpect", "pyyaml", "requests", "termcolor"],
+    install_requires=["attrs", "babel", "pexpect", "pyyaml", "requests", "termcolor"],
     keywords=["push50"],
     name="push50",
     python_requires=">= 3.6",
     packages=["push50"],
     url="https://github.com/cs50/push50",
     version="1.0.0",
+    package_data={'': ['locale/*/*/*.mo', 'locale/*/*/*.po']},
     include_package_data=True
 )
