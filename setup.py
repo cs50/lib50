6b94953569791f4ab6b896f8b3e2e42ef64a4edc
Chad Sharp <chad@cs50.harvard.edu>
2018-08-08 17:08:38 -0400

    version++

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     python_requires=">= 3.6",
     packages=["lib50"],
     url="https://github.com/cs50/lib50",
-    version="1.0.0",
+    version="1.0.1",
     include_package_data=True
 )
