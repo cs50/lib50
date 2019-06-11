c11a6b6f9ece6bebe92e1719a3c094188fe3aa23
jelleas <jelle.van.assema@gmail.com>
2019-06-11 11:45:55 -0400

    bump version

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     python_requires=">= 3.6",
     packages=["lib50"],
     url="https://github.com/cs50/lib50",
-    version="1.0.2",
+    version="1.1.0",
     include_package_data=True
 )
