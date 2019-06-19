6ce783c679b37d3dcaccbedc146bb9fc5c2c9b28
Jelle van Assema <jelle.van.assema@gmail.com>
2019-06-19 15:29:43 -0400

    v1.1.0 (#11)
    * add comments
    
    * unexpand user in MissingFileErrors
    
    * remove pathlib import
    
    * Loader
    
    * set scoped tags to false
    
    * make private modules private
    
    * files() now supports custom tags
    
    * local() and push() are again unaware of check50/submit50
    
    * optional ! for TaggedValue
    
    * -f, necessary if you started with .yml checks
    
    * encode utf8
    
    * fix n stars with >1B chars
    
    * support for .yml
    
    * TaggedValue cleanup
    
    * bump version
    
    * lib50.local does not check config
    
    * add version requirements
    
    * Introduced TimeoutError and ConnectionError
    
    * payload on MissingFilesError
    
    * add get_local_path/set_local_path
    
    * add DEFAULT_PUSH_ORG and other stuff
    
    * added validate flag and config tests
    
    * fix tests
    
    * fetch_config
    
    * fix args bug
    
    * update versions
    
    * move validate param to load
    
    * added get_local_slugs
    
    * mv validate
    
    * look for 'Username for' when doing git ls-remote
    
    * get_local_slugs test
    
    * mmmm generators
    
    * query github for id
    
    * rip generators
    
    * socket
    
    * that's fine it'll work
    
    * update some localizaions, add AUTH_URL
    
    * fix localization
    
    * fix more localizations
    
    * added missing translations courtesy of coderigo17
    
    * fix parens
    
    * added punctuation
    
    * rm readline
    
    * always raise an error incase of two configs
    
    * comment
    
    * progress bars ftw
    
    * add .ssh to always_exclude files
    
    * ignore . by default
    
    * default in lab50 tests
    
    * added _match_files
    
    * remove hidden files
    
    * remove always_exclude
    
    * rm always_exclude test
    
    * allow pyyaml 3
    
    * swap Levenshtein for jellyfish
    
    * update tests to point to tests branch

---
+++ lib50/_errors.py
@@ -0,0 +1,35 @@
+import os
+from . import _
+
+__all__ = ["Error", "InvalidSlugError", "MissingFilesError", "InvalidConfigError", "MissingToolError", "TimeoutError", "ConnectionError"]
+
+class Error(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.payload = {}
+
+class InvalidSlugError(Error):
+    pass
+
+class MissingFilesError(Error):
+    def __init__(self, files):
+        cwd = os.getcwd().replace(os.path.expanduser("~"), "~", 1)
+        super().__init__("{}\n{}\n{}".format(
+            _("You seem to be missing these required files:"),
+            "\n".join(files),
+            _("You are currently in: {}, did you perhaps intend another directory?".format(cwd))
+        ))
+        self.payload.update(files=files, dir=cwd)
+
+
+class InvalidConfigError(Error):
+    pass
+
+class MissingToolError(InvalidConfigError):
+    pass
+
+class TimeoutError(Error):
+    pass
+
+class ConnectionError(Error):
+    pass
