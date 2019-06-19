30ca93c5f119f0517ce7bd2c3ae7bbc15bd3f97a
Chad Sharp <chad@cs50.harvard.edu>
2019-06-19 15:06:33 -0400

    add tests

---
+++ bar/__init__.py
@@ -0,0 +1,20 @@
+import check50
+import check50.c
+
+@check50.check()
+def compiles():
+    """hello.c compiles."""
+    check50.c.compile("hello.c", lcs50=True)
+
+@check50.check(compiles)
+def prints_hello():
+    """prints "hello, world\\n" """
+    from re import match
+
+    expected = "[Hh]ello, world!?\n"
+    actual = check50.run("./hello").stdout()
+    if not match(expected, actual):
+        help = None
+        if match(expected[:-1], actual):
+            help = r"did you forget a newline ('\n') at the end of your printf string?"
+        raise check50.Mismatch("hello, world\n", actual, help=help)
