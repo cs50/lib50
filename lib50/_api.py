7de6b178fd4023f144e694686dd3ada219b8955b
Chad Sharp <chad@cs50.harvard.edu>
2019-07-29 16:20:49 -0400

    push over 443

--- lib50/_api.py
+++ lib50/_api.py
@@ -777,7 +777,7 @@ def _lfs_add(files, git):
 def _authenticate_ssh(org):
     """Try authenticating via ssh, if succesful yields a User, otherwise raises Error."""
     # Require ssh-agent
-    child = pexpect.spawn("ssh -T git@github.com", encoding="utf8")
+    child = pexpect.spawn("ssh -p443 -T git@ssh.github.com", encoding="utf8")
     # GitHub prints 'Hi {username}!...' when attempting to get shell access
     try:
         i = child.expect(["Hi (.+)! You've successfully authenticated",
@@ -796,7 +796,7 @@ def _authenticate_ssh(org):
         return None
 
     return User(name=username,
-                repo=f"git@github.com:{org}/{username}",
+                repo=f"ssh://git@ssh.github.com:443/{org}/{username}",
                 org=org)
 
 
