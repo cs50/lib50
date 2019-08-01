93f622c938b10ef31cbe102383e54798a363a556
jelleas <jelle.van.assema@gmail.com>
2019-08-01 11:21:53 -0400

    version_n--

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     python_requires=">= 3.6",
     packages=["lib50"],
     url="https://github.com/cs50/lib50",
-    version="1.1.10",
+    version="1.1.9",
     include_package_data=True
 )
