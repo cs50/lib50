cb178791e9a7544ea940e5d6be0ca0aab1535f49
Chad Sharp <chad@cs50.harvard.edu>
2020-04-08 16:18:07 -0400

    check honesty prompt is not false first

--- lib50/_api.py
+++ lib50/_api.py
@@ -69,7 +69,7 @@ def push(tool, slug, config_loader, repo=None, data=None, prompt=lambda question
     with authenticate(remote["org"], repo=repo) as user, prepare(tool, slug, user, included):
 
         # Show any prompt if specified
-        if prompt(honesty, included, excluded):
+        if honesty and prompt(honesty, included, excluded):
             username, commit_hash = upload(slug, user, tool, data)
             format_dict = {"username": username, "slug": slug, "commit_hash": commit_hash}
             message = remote["message"].format(results=remote["results"].format(**format_dict), **format_dict)
