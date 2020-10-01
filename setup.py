12ef708cf7d46fed1725612f88f3295470e0ea45
jelleas <jelle.van.assema@gmail.com>
2020-10-01 17:18:30 +0200

    accept attrs version >=20, minor version bump

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ setup(
     },
     license="GPLv3",
     description="This is lib50, CS50's own internal library used in many of its tools.",
-    install_requires=["attrs>=18.1,<20", "pexpect>=4.6,<5", "pyyaml>=3.10,<6", "requests>=2.13,<3", "termcolor>=1.1,<2", "jellyfish>=0.7,<1", "cryptography>=2.7"],
+    install_requires=["attrs>=18.1", "pexpect>=4.6,<5", "pyyaml>=3.10,<6", "requests>=2.13,<3", "termcolor>=1.1,<2", "jellyfish>=0.7,<1", "cryptography>=2.7"],
     keywords=["lib50"],
     name="lib50",
     python_requires=">= 3.6",
     packages=["lib50"],
     url="https://github.com/cs50/lib50",
-    version="2.0.8",
+    version="2.0.9",
     include_package_data=True
 )
