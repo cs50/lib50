c6156d7ab675386de167b3ec6895f01c1f5cc031
Kareem Zidane <kzidane@cs50.harvard.edu>
2021-07-16 23:29:44 -0400

    Deploy with GitHub Actions

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
     python_requires=">= 3.6",
     packages=["lib50"],
     url="https://github.com/cs50/lib50",
-    version="2.0.9",
+    version="2.0.10",
     include_package_data=True
 )
