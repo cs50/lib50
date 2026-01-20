79f3ab7f1ba69e5ca8fbf9c7afec00ee2cdeddb0
Rongxin Liu <rongxinliu.dev@gmail.com>
2026-01-20 13:50:10 +0000

    bump version to 3.2.1 in setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
     python_requires=">= 3.10",
     packages=["lib50"],
     url="https://github.com/cs50/lib50",
-    version="3.2.0",
+    version="3.2.1",
     include_package_data=True
 )
